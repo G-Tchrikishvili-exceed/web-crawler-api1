a017304156fb158ed840937408928f34bc509ce2
V-Jgenti-exceed <62645242+V-Jgenti-exceed@users.noreply.github.com>
2020-07-21 17:32:20 +0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.render('index', {
-    title: 'WEB CRAWLER API',
-    description: 'you can use this urls:',
-  });
+//   res.render('index', {
+//     title: 'WEB CRAWLER API',
+//     description: 'you can use this urls:',
+//   });
   res.status(200).send('health_CHECK is ok');
 });
 
