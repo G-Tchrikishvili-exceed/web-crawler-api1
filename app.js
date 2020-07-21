1ca7c33e96a99f70e3dd5044c508c198c4bbd77b
V-Jgenti-exceed <62645242+V-Jgenti-exceed@users.noreply.github.com>
2020-07-21 17:33:40 +0400

    Update app.js

--- app.js
+++ app.js
@@ -79,4 +79,15 @@ app.use(function (err, req, res, next) {
   });
 });
 
-module.exports = app;
+var debug = require('debug')('my-application');
+// var app = require('../app');
+
+app.set('port', process.env.PORT || 5000);
+console.log('@@@@@@hi from www');
+var server = app.listen(app.get('port'), function () {
+  debug('Express server listening on port ' + server.address().port);
+  console.log('server is up and working correctly');
+});
+
+
+// module.exports = app;
